c7b29914ab2cb24551fbd0fbae45ee5b28b3dcaa
Настя Терехова <taa73825564@gmail.com>
2024-10-05 22:32:25 +0300

    add ordering for recipes 3

--- backend/api/models.py
+++ backend/api/models.py
@@ -77,7 +77,7 @@ class Recipe(models.Model):
                                       verbose_name='Дата публикации')
 
     class Meta:
-        ordering = ['created_at', ]
+        ordering = ['created_at', 'name', 'author']
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
         default_related_name = 'recipes'
