6f793a4053749a5c02e699e3c3ff2f89d864435a
Настя Терехова <taa73825564@gmail.com>
2024-10-05 22:10:45 +0300

    change admin in api and users

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,16 +1,65 @@
 from django.contrib import admin
+from users.admin import BaseAdmin
 
-from .models import Favorite, Ingredients, Recipe, ShoppingCart, Tags
-
-
-class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'author')
-    list_display_links = ('id', 'author')
-    search_fields = ('name', 'author')
+from .models import (Favorite, Ingredients, IngredientsRecipe, Recipe,
+                     ShoppingCart, Tags)
 
 
-admin.site.register(Ingredients)
-admin.site.register(Tags)
-admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(Favorite)
-admin.site.register(ShoppingCart)
+class IngredientInRecipeInline(admin.TabularInline):
+    model = IngredientsRecipe
+    extra = 0
+    min_num = 1
+
+
+@admin.register(Ingredients)
+class IngredientAdmin(BaseAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name', )
+    list_filter = ('name', )
+    inlines = (IngredientInRecipeInline, )
+
+
+@admin.register(Tags)
+class TagAdmin(BaseAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name', )
+    list_filter = ('name', )
+
+
+@admin.register(Recipe)
+class RecipeAdmin(BaseAdmin):
+    list_display = (
+        'name',
+        'author',
+        'text',
+        'cooking_time',
+        'created_at',
+        'favorites_count',
+        'display_ingredients'
+    )
+    search_fields = ('name', 'author__username', 'favorites_count')
+    list_filter = ('author', 'name', 'tags')
+    inlines = (IngredientInRecipeInline,)
+
+    @admin.display(description="Число добавлений в избранное")
+    def favorites_count(self, obj):
+        return obj.favorites.count()
+
+    @admin.display(description="Отображение ингредиентов")
+    def display_ingredients(self, recipe):
+        return ', '.join([
+            ingredients.name for ingredients in recipe.ingredients.all()])
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(BaseAdmin):
+    list_display = ('user', 'recipe')
+    list_filter = ('user', 'recipe')
+    search_fields = ('user',)
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(BaseAdmin):
+    list_display = ('user', 'recipe')
+    list_filter = ('user', 'recipe')
+    search_fields = ('user',)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,5 +2,33 @@ from django.contrib import admin
 
 from .models import Follow, User
 
-admin.site.register(User)
-admin.site.register(Follow)
+
+class BaseAdmin(admin.ModelAdmin):
+    """Базовый класс для настройки административной панели Django."""
+
+    empty_value_display = '-'
+    list_per_page = 20
+
+
+@admin.register(User)
+class UserAdmin(BaseAdmin):
+    list_display = (
+        'username',
+        'first_name',
+        'last_name',
+        'email',
+    )
+    search_fields = ('username', 'email',)
+    list_filter = ('username', 'email',)
+    ordering = ('username',)
+
+
+@admin.register(Follow)
+class FollowAdmin(BaseAdmin):
+    list_display = (
+        'user',
+        'following',
+    )
+    search_fields = ('user__username', 'following__username',)
+    list_filter = ('user', 'following',)
+    ordering = ('user',)
